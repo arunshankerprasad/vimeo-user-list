fec5fcbeec73bd2e3ecb5a283be0601da6db72b5
Arun Shanker Prasad <arunshankerprasad@gmail.com>
2013-02-26 01:34:01 +0530

    Initial

---
+++ wsgi.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'vimeotest')))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'vimeotest.settings'
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
