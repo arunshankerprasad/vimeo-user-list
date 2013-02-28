b55bc07fd8ffa0140e8feae26c4958f106bded45
Arun Shanker Prasad <arunshankerprasad@gmail.com>
2013-03-01 01:38:43 +0530

    Modified stuff

--- vimeotest/user_list/models.py
+++ vimeotest/user_list/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=400)
+    url = models.CharField(max_length=500)
+    is_paying_user = models.BooleanField()
+    has_video_in_staff_pick = models.BooleanField()
+    has_atleast_one_video = models.BooleanField()
--- vimeotest/user_list/views.py
+++ vimeotest/user_list/views.py
@@ -1,10 +1,11 @@
-from django.template import Context, loader
-from datetime import datetime
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_protect, csrf_exempt
 
+from datetime import datetime
 import urllib2
 from bs4 import BeautifulSoup
+
+from django.template import Context, loader
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
 
 # plug soupselect
 # https://code.google.com/p/soupselect/
@@ -24,20 +25,29 @@ def get_users(request):
     """ List all users View """
 
     users = []
+    page = 0
 
     if request.POST:
-
         page = urllib2.urlopen('http://vimeo.com/channels/staffpicks/subscribers').read()
         soup = BeautifulSoup(page)
         soup.prettify()
 
         for a in soup.findSelect("ol#browse_list li a"):
-            users.append(a['title'])
+            u = User(
+                name=a['title'],
+                url='',
+                is_paying_user=False,
+                has_video_in_staff_pick=False,
+                has_atleast_one_video=False
+            )
+            u.put()
+            users.append(u)
 
     t = loader.get_template('get_users.html')
     c = Context({
         'current_time': datetime.now(),
-        'users':        users
+        'users':        users,
+        'page':         page
     })
 
     return HttpResponse(t.render(c))
